b4e6ec09ea025b61e46b1e7a66bc6865689213ff
dinshaw <dinshaw.raje@gmail.com>
2019-08-05 10:55:03 +0530

    dropdown functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employee-app';
+   months = ["January", "Feburary", "March", "April", "May", 
+            "June", "July", "August", "September",
+            "October", "November", "December"];
+	selectedDay: string = '';
+  //event handler for the select element's change event
+  selectChangeHandler (event: any) {
+    //update the ui
+    this.selectedDay = event.target.value;
+    console.log(event.target.value);
+  }
 }
